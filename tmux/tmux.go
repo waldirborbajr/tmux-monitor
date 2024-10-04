8b62061bcc72070fa58b0b7e4338bc4472848401
Waldir Borba Junior <wborbajr@gmail.com>
2024-10-03 22:33:48 -0300

    add(tmux): add package to control tmux flags

---
+++ tmux/tmux.go
@@ -0,0 +1,23 @@
+package tmux
+
+import "os/exec"
+
+// Command runs a tmux command with given arguments
+func Command(subcommand string, args ...string) error {
+	command := []string{subcommand}
+	for _, arg := range args {
+		command = append(command, arg)
+	}
+
+	return exec.Command("tmux", command...).Start()
+}
+
+// DisplayMessage runs tmux display-message with the given string
+func DisplayMessage(message string) error {
+	return Command("display-message", message)
+}
+
+// RefreshClient tells tmux to refresh the display, including the status-bar
+func RefreshClient(args ...string) error {
+	return Command("refresh-client", args...)
+}
